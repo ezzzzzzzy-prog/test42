ef65e34ed26fa3abff75452fd0cd8fc03c49785a
vboxuser <nora.baali@epita.fr>
2026-01-19 23:36:46 +0000

    double quotes fini et teste

--- src/expansion.c
+++ src/expansion.c
@@ -1,68 +1,73 @@
+#include "expansion.h"
+
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include "special.h"
-#include "expansion.h"
 
-static int agrandir( char **res, size_t *size) //c'est une fonction pour agrandir
+#include "special.h"
+
+static int agrandir(char **res, size_t *size) // c'est une fonction pour
+                                              // agrandir
 {
-        *size *= 2;
-       char *tmp = realloc(*res, *size);
-       if(!tmp)
-       {
-                free(*res);
-                return 0;
-       }
-       *res = tmp;
-       return 1;
+    *size *= 2;
+    char *tmp = realloc(*res, *size);
+    if (!tmp)
+    {
+        free(*res);
+        return 0;
+    }
+    *res = tmp;
+    return 1;
 }
 
-
-static int copy(const char *val, char **res, size_t *j, size_t *size) //pour copier
+static int copy(const char *val, char **res, size_t *j,
+                size_t *size) // pour copier
 {
-        while(*val)
+    while (*val)
+    {
+        if (*j >= *size - 1)
         {
-                 if(*j >= *size -1)
-                 {
-                         if(!agrandir(res,size))
-                         {
-                                 return 0;
-                         }
-                 }
-                 (*res)[(*j)++] = *val++;
-         }
-         return 1;
+            if (!agrandir(res, size))
+            {
+                return 0;
+            }
+        }
+        (*res)[(*j)++] = *val++;
+    }
+    return 1;
 }
 
 static char *var_normal(const char *word, size_t *i, int is_bra)
 {
-        size_t start = *i;
-        if(is_bra)
+    size_t start = *i;
+    if (is_bra)
+    {
+        while (word[*i] && word[*i] != '}')
         {
-                while(word[*i] && word[*i] != '}')
-                {
-                        (*i)++;
-                }
+            (*i)++;
         }
-        else
+    }
+    else
+    {
+        while (word[*i] && word[*i] != ' ' && word[*i] != '\t'
+               && word[*i] != '\n' && word[*i] != '$' && word[*i] != '"'
+               && word[*i] != '\'')
         {
-                while(word[*i] && word[*i] != ' ' && word[*i] != '\t' && word[*i] != '\n' && word[*i] != '$' && word[*i] != '"' && word[*i] != '\'')
-                {
-                                    (*i)++;
-                }
+            (*i)++;
         }
-        size_t len = *i - start;
-        char *name = malloc(len +1);
-        if(!name)
-        {
-                //free(res);
-                return NULL;
-        }
-        memcpy(name, word + start, len);
-        name[len] = '\0';
-        return name;
+    }
+    size_t len = *i - start;
+    char *name = malloc(len + 1);
+    if (!name)
+    {
+        // free(res);
+        return NULL;
+    }
+    memcpy(name, word + start, len);
+    name[len] = '\0';
+    return name;
 }
 static const char *find_var(const char *name, struct variable *var)
 {
@@ -78,141 +83,192 @@ static const char *find_var(const char *name, struct variable *var)
     return "";
 }
 
-static int expand_special(struct special *spe, char spe_char, char **res, size_t *j, size_t *size)
+static int expand_special(struct special *spe, char spe_char, char **res,
+                          size_t *j, size_t *size)
 {
-	if(spe && spe->args)
-		return 1;
+    if (spe && spe->args)
+        return 1;
 
-                            for(int l = 0; l < spe->argc_count; l++)
-                            {
-                                    if(!copy(spe->args[l], res, j, size))
-                                    {
-                                            return 0;
-                                    }
-                                    if(spe_char == '@' && l +1 < spe->argc_count)
-                                    {
-                                            if( *j >= *size - 1 && !agrandir(res, size))
-                                            {
-                                                    return 0;
-                                            }
-                                            (*res)[(*j)++] = ' ';
-                                    }
-                            }
-			    return 1;
-}
-
-static int expand_braced_var(struct parser *parser, const char *word, size_t *i, char **res, size_t *j, size_t *size)
-{
-	(*i)++;
-	char *name = var_normal(word, i, 1);
-                    if(!name)
-                    {
-                            //free(res);
-                            return 0;
-                    }
-                    const char *val = find_var(name, parser->var);
-                    if(!copy(val, res,j, size))
-                    {
-                            free(name);
-                            //free(res);
-                            return 0;
-                    }
-                    free(name);
-		    return 1;
-}
-
-static int expand_simple_var(struct parser *parser, const char *word, size_t *i, char **res, size_t *j, size_t *size)
-{
-	char *name = var_normal(word, i, 0);
-            if(!name)
+    for (int l = 0; l < spe->argc_count; l++)
+    {
+        if (!copy(spe->args[l], res, j, size))
+        {
+            return 0;
+        }
+        if (spe_char == '@' && l + 1 < spe->argc_count)
+        {
+            if (*j >= *size - 1 && !agrandir(res, size))
             {
-                    //free(res);
-                    return 0;
+                return 0;
             }
-            const char *val = find_var(name, parser->var);
-            if(!copy(val, res, j, size))
-            {
-                    free(name);
-                    //free(res);
-                    return 0;
-            }
-            free(name);
-	    return 1;
+            (*res)[(*j)++] = ' ';
+        }
+    }
+    return 1;
 }
 
-char *expand(struct parser *parser,struct special *spe, const char *word)
+static int expand_braced_var(struct parser *parser, const char *word, size_t *i,
+                             char **res, size_t *j, size_t *size)
+{
+    (*i)++;
+    char *name = var_normal(word, i, 1);
+    if (!name)
+    {
+        // free(res);
+        return 0;
+    }
+    const char *val = find_var(name, parser->var);
+    if (!copy(val, res, j, size))
+    {
+        free(name);
+        // free(res);
+        return 0;
+    }
+    free(name);
+    return 1;
+}
+
+static int expand_simple_var(struct parser *parser, const char *word, size_t *i,
+                             char **res, size_t *j, size_t *size)
+{
+    char *name = var_normal(word, i, 0);
+    if (!name)
+    {
+        // free(res);
+        return 0;
+    }
+    const char *val = find_var(name, parser->var);
+    if (!copy(val, res, j, size))
+    {
+        free(name);
+        // free(res);
+        return 0;
+    }
+    free(name);
+    return 1;
+}
+
+static int expand_double_quot(const char *word, size_t *i, char **res,
+                              size_t *j, size_t *size)
+{
+    (*i)++;
+    char n = word[*i];
+    if (n == '$' || n == '`' || n == '"' || n == '\\' || n == '\n')
+    {
+        if (n == '\n')
+        {
+            (*i)++;
+            return 1;
+        }
+        if (*j >= *size - 1 && !agrandir(res, size))
+            return 0;
+        (*res)[(*j)++] = word[(*i)++];
+        return 1;
+    }
+    if (*j >= *size - 1 && !agrandir(res, size))
+        return 0;
+    (*res)[(*j)++] = '\\';
+    return 1;
+}
+
+char *expand(struct parser *parser, struct special *spe, const char *word)
 {
     if (!word || !parser)
     {
         return NULL;
     }
     size_t len = strlen(word);
+    // int quot = 0;
+    size_t i = 0;
+    if (len >= 2 && word[0] == '"' && word[len - 1] == '"')
+    {
+        // quot = 1;
+        i = 1;
+        len = len - 1;
+    }
+    if (len >= 2 && word[0] == '\'' && word[len - 1] == '\'')
+    {
+        // quot = 1;
+        i = 1;
+        len = len - 1;
+    }
+
     size_t size = len * 2 + 1;
     char *res = malloc(size);
     if (!res)
     {
         return NULL;
     }
-    size_t i = 0;
     size_t j = 0;
     while (word[i])
     {
-        if (word[i] == '$')
+        if (word[i] == '\\')
+        {
+            if (!expand_double_quot(word, &i, &res, &j, &size))
+            {
+                free(res);
+                return NULL;
+            }
+        }
+        else if (word[i] == '"' || word[i] == '\'')
+            i++;
+        else if (word[i] == '$')
         {
             i++;
-            if(!word[i])
+            if (!word[i])
             {
-                    break;
+                break;
             }
-            if(word[i] == '@' || word[i] == '*')
+            if (word[i] == '@' || word[i] == '*')
             {
-                    char spe_char = word[i++];
-		    if (!expand_special(spe,spe_char, &res, &j, &size))
-			    return NULL;
-                    /*if(spe && spe->args)
-                    {
-                            for(int l = 0; l < spe->argc_count; l++)
-                            {
-                                    if(!copy(spe->args[l], &res, &j, &size))
-                                    {
-                                            return NULL;
-                                    }
-                                    if(spe_char == '@' && l +1 < spe->argc_count)
-                                    {
-                                            if( j >= size - 1 && !agrandir(&res, &size))
-                                            {
-                                                    return NULL;
-                                            }
-                                            res[j++] = ' ';
-                                    }
-                            }
-                    }
-                    continue;*/
+                char spe_char = word[i++];
+                if (!expand_special(spe, spe_char, &res, &j, &size))
+                    return NULL;
+                /*if(spe && spe->args)
+                {
+                        for(int l = 0; l < spe->argc_count; l++)
+                        {
+                                if(!copy(spe->args[l], &res, &j, &size))
+                                {
+                                        return NULL;
+                                }
+                                if(spe_char == '@' && l +1 < spe->argc_count)
+                                {
+                                        if( j >= size - 1 && !agrandir(&res,
+                &size))
+                                        {
+                                                return NULL;
+                                        }
+                                        res[j++] = ' ';
+                                }
+                        }
+                }
+                continue;*/
             }
-            if(word[i] == '{')
+            if (word[i] == '{')
             {
-                    /*i++;
-                    char *name = var_normal(word, &i, 1);
-                    if(!name)
-                    {
-                            free(res);
-                            return NULL;
-                    }
-                    const char *val = find_var(name, parser->var);
-                    if(!copy(val, &res, &j, &size))
-                    {
-                            free(name);
-                            free(res);
-                            return NULL;
-                    }
-                    free(name);*/
-		    if (!expand_braced_var(parser, word, &i, &res, &j, &size))
-			    return NULL;
-                    if(word[i] == '}')
-                    {
-                            i++;
-                    }
-                    continue;
+                /*i++;
+                char *name = var_normal(word, &i, 1);
+                if(!name)
+                {
+                        free(res);
+                        return NULL;
+                }
+                const char *val = find_var(name, parser->var);
+                if(!copy(val, &res, &j, &size))
+                {
+                        free(name);
+                        free(res);
+                        return NULL;
+                }
+                free(name);*/
+                if (!expand_braced_var(parser, word, &i, &res, &j, &size))
+                    return NULL;
+                if (word[i] == '}')
+                {
+                    i++;
+                }
+                continue;
             }
             /*char *name = var_normal(word, &i, 0);
             if(!name)
@@ -228,16 +284,16 @@ char *expand(struct parser *parser,struct special *spe, const char *word)
                     return NULL;
             }
             free(name);*/
-	    if (!expand_simple_var(parser, word, &i, &res, &j, &size))
-		    return NULL;
+            if (!expand_simple_var(parser, word, &i, &res, &j, &size))
+                return NULL;
         }
         else
         {
-                if(j >= size - 1 && !agrandir(&res, &size))
-                {
-                        return NULL;
-                }
-                res[j++] = word[i++];
+            if (j >= size - 1 && !agrandir(&res, &size))
+            {
+                return NULL;
+            }
+            res[j++] = word[i++];
         }
     }
     res[j] = '\0';
--- src/lexer.c
+++ src/lexer.c
@@ -73,7 +73,7 @@ static struct token *new_tok(enum type type, char *val)
 
 static int skip(void)
 {
-	int c = io_backend_next();
+    int c = io_backend_next();
 
     while (c == ' ' || c == '\t')
         c = io_backend_next();
@@ -82,7 +82,7 @@ static int skip(void)
 
 static struct token *simple_tok(int c)
 {
-	 if (c == EOF)
+    if (c == EOF)
         return new_tok(TOK_EOF, NULL);
     if (c == '!')
         return new_tok(TOK_NOT, NULL);
@@ -96,127 +96,158 @@ static struct token *simple_tok(int c)
 
 static struct token *pipe_tok(void)
 {
-	 if (io_backend_peek() == '|')
-        {
-            io_backend_next();
-            return new_tok(TOK_OR, NULL);
-        }
-        return new_tok(TOK_IN, NULL);
+    if (io_backend_peek() == '|')
+    {
+        io_backend_next();
+        return new_tok(TOK_OR, NULL);
+    }
+    return new_tok(TOK_IN, NULL);
 }
 
 static struct token *redir_out_tok(void)
 {
-	if (io_backend_peek() == '>')
-        {
-            io_backend_next();
-            return new_tok(TOK_REDIR_APP, NULL);
-        }
-        if (io_backend_peek() == '&')
-        {
-            io_backend_next();
-            return new_tok(TOK_REDIR_DUP_OUT, NULL);
-        }
-        if (io_backend_peek() == '|')
-        {
-            io_backend_next();
-            return new_tok(TOK_REDIR_FORC_OUT, NULL);
-        }
-        return new_tok(TOK_REDIR_OUT, NULL);
+    if (io_backend_peek() == '>')
+    {
+        io_backend_next();
+        return new_tok(TOK_REDIR_APP, NULL);
+    }
+    if (io_backend_peek() == '&')
+    {
+        io_backend_next();
+        return new_tok(TOK_REDIR_DUP_OUT, NULL);
+    }
+    if (io_backend_peek() == '|')
+    {
+        io_backend_next();
+        return new_tok(TOK_REDIR_FORC_OUT, NULL);
+    }
+    return new_tok(TOK_REDIR_OUT, NULL);
 }
 
 static struct token *redir_in_tok(void)
 {
-	if (io_backend_peek() == '&')
-        {
-            io_backend_next();
-            return new_tok(TOK_REDIR_DUP_IN, NULL);
-        }
-        if (io_backend_peek() == '>')
-        {
-            io_backend_next();
-            return new_tok(TOK_REDIR_RW, NULL);
-        }
-        return new_tok(TOK_REDIR_IN, NULL);
+    if (io_backend_peek() == '&')
+    {
+        io_backend_next();
+        return new_tok(TOK_REDIR_DUP_IN, NULL);
+    }
+    if (io_backend_peek() == '>')
+    {
+        io_backend_next();
+        return new_tok(TOK_REDIR_RW, NULL);
+    }
+    return new_tok(TOK_REDIR_IN, NULL);
 }
 
 static struct token *comment_tok(int c)
 {
-	while (c != EOF && c != '\n')
-            c = io_backend_next();
-        return new_tok(c == '\n' ? TOK_NEWLINE : TOK_EOF, NULL);
+    while (c != EOF && c != '\n')
+        c = io_backend_next();
+    return new_tok(c == '\n' ? TOK_NEWLINE : TOK_EOF, NULL);
+}
+
+static struct token *double_quot_tok(void)
+{
+    int c = 0;
+    int cap = 64;
+    int s = 0;
+    char *buf = malloc(cap);
+    if (!buf)
+    {
+        return NULL;
+    }
+    while ((c = io_backend_next()) != EOF && c != '"')
+    {
+        if (c == '\\')
+        {
+            int n = io_backend_peek();
+            if (n == '$' || n == '`' || n == '"' || n == '\\' || n == '\n')
+            {
+                buf = append_char(buf, &s, &cap, '\\');
+                buf = append_char(buf, &s, &cap, io_backend_next());
+            }
+            else
+                buf = append_char(buf, &s, &cap, '\\');
+        }
+        else
+            buf = append_char(buf, &s, &cap, c);
+    }
+    struct token *tok = new_tok(TOK_WORD, my_strdup(buf));
+    free(buf);
+    return tok;
 }
 
 static struct token *quot_tok(void)
 {
-	int c;
-	int cap = 64;
-        int s = 0;
-        char *buf = malloc(cap);
-        // int i = 0;
-        while ((c = io_backend_next()) != EOF && c != '\'')
-        {
-            buf = append_char(buf, &s, &cap, c);
-        }
-        struct token *tok = new_tok(TOK_WORD, my_strdup(buf));
-        free(buf);
-        return tok;
+    int c;
+    int cap = 64;
+    int s = 0;
+    char *buf = malloc(cap);
+    // int i = 0;
+    while ((c = io_backend_next()) != EOF && c != '"')
+    {
+        buf = append_char(buf, &s, &cap, c);
+    }
+    struct token *tok = new_tok(TOK_WORD, my_strdup(buf));
+    free(buf);
+    return tok;
 }
 
 static struct token *word_tok(char *buf)
 {
-	 if (strcmp(buf, "if") == 0)
+    if (strcmp(buf, "if") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_IF, NULL);
     }
     if (strcmp(buf, "then") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_THEN, NULL);
     }
     if (strcmp(buf, "elif") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_ELIF, NULL);
     }
     if (strcmp(buf, "else") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_ELSE, NULL);
     }
     if (strcmp(buf, "fi") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_FI, NULL);
     }
     if (strcmp(buf, "while") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_WHILE, NULL);
     }
     if (strcmp(buf, "until") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_UNTIL, NULL);
     }
     if (strcmp(buf, "for") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_FOR, NULL);
     }
     if (strcmp(buf, "do") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_DO, NULL);
     }
     if (strcmp(buf, "done") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_DONE, NULL);
     }
     if (strcmp(buf, "in") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_IN, NULL);
     }
     struct token *tok = new_tok(TOK_WORD, my_strdup(buf));
@@ -226,7 +257,7 @@ static struct token *word_tok(char *buf)
 
 static struct token *read_tok(int c)
 {
-	int cap = 64;
+    int cap = 64;
     int s = 0;
     char *buf = malloc(cap);
     while (c != EOF && !isspace(c) && c != ';' && c != '|' && c != '<'
@@ -248,8 +279,8 @@ static struct token *build(void)
 
     while (c == ' ' || c == '\t')
         c = io_backend_next();*/
-	int c = skip();
-	struct token *tok;
+    int c = skip();
+    struct token *tok;
 
     /*if (c == EOF)
         return new_tok(TOK_EOF, NULL);
@@ -260,8 +291,8 @@ static struct token *build(void)
 
     if (c == ';')
         return new_tok(TOK_SEMI, NULL);*/
-	if ((tok = simple_tok(c)))
-		return tok;
+    if ((tok = simple_tok(c)))
+        return tok;
 
     if (c == '|')
     {
@@ -271,7 +302,11 @@ static struct token *build(void)
             return new_tok(TOK_OR, NULL);
         }
         return new_tok(TOK_IN, NULL);*/
-	    return pipe_tok();
+        return pipe_tok();
+    }
+    if (c == '"')
+    {
+        return double_quot_tok();
     }
     if (c == '>')
     {
@@ -291,7 +326,7 @@ static struct token *build(void)
             return new_tok(TOK_REDIR_FORC_OUT, NULL);
         }
         return new_tok(TOK_REDIR_OUT, NULL);*/
-	    return redir_out_tok();
+        return redir_out_tok();
     }
 
     if (c == '<')
@@ -307,7 +342,7 @@ static struct token *build(void)
             return new_tok(TOK_REDIR_RW, NULL);
         }
         return new_tok(TOK_REDIR_IN, NULL);*/
-	    return redir_in_tok();
+        return redir_in_tok();
     }
 
     if (c == '#')
@@ -315,7 +350,7 @@ static struct token *build(void)
         /*while (c != EOF && c != '\n')
             c = io_backend_next();
         return new_tok(c == '\n' ? TOK_NEWLINE : TOK_EOF, NULL);*/
-	    return comment_tok(c);
+        return comment_tok(c);
     }
     if (c == '\'')
     {
@@ -328,9 +363,9 @@ static struct token *build(void)
             buf = append_char(buf, &s, &cap, c);
         }
         struct token *tok = new_tok(TOK_WORD, my_strdup(buf));
-	free(buf);
-	return tok;*/
-	    return quot_tok();
+    free(buf);
+    return tok;*/
+        return quot_tok();
     }
     if (c == '&')
     {
@@ -340,8 +375,8 @@ static struct token *build(void)
             return new_tok(TOK_AND, NULL);
         }
         struct token *tok = new_tok(TOK_WORD, my_strdup("&"));
-	//free(buf);
-	return tok;
+        // free(buf);
+        return tok;
     }
     /*int cap = 64;
     int s = 0;
@@ -359,64 +394,64 @@ static struct token *build(void)
 
     if (strcmp(buf, "if") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_IF, NULL);
     }
     if (strcmp(buf, "then") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_THEN, NULL);
     }
     if (strcmp(buf, "elif") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_ELIF, NULL);
     }
     if (strcmp(buf, "else") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_ELSE, NULL);
     }
     if (strcmp(buf, "fi") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_FI, NULL);
     }
     if (strcmp(buf, "while") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_WHILE, NULL);
     }
     if (strcmp(buf, "until") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_UNTIL, NULL);
     }
     if (strcmp(buf, "for") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_FOR, NULL);
     }
     if (strcmp(buf, "do") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_DO, NULL);
     }
     if (strcmp(buf, "done") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_DONE, NULL);
     }
     if (strcmp(buf, "in") == 0)
     {
-	    free(buf);
+        free(buf);
         return new_tok(TOK_IN, NULL);
     }
 
     struct token *tok = new_tok(TOK_WORD, my_strdup(buf));
     free(buf);
     return tok;*/
-	    return read_tok(c);
+    return read_tok(c);
 }
 
 struct token *peek(struct lexer *lex)
